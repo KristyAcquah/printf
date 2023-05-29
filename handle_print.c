5fc1a4dc9521a23412273e6cb82be0f9c7c4c155
KristyAcquah <akosuaacquah99@gmail.com>
2023-05-29 03:13:50 -0700

    update

--- handle_print.c
+++ handle_print.c
@@ -18,11 +18,13 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 {
 	int i, unknown_len = 0,
 	    printed_char = -1;
-	fmt_t fmt_types[i] = {'c', print_char}, {'s', print_string}, {'%', print_percent},
-		{'i', print_int}, {"d", print_int}, {'b', print_binary},
-		{'u', print_unsigned}, {'0', print_octa}, {'x', print_hexadecimal},
-		{'X', print_hex_upper}, {'p', print_pointer}, {'S', print_hex_printable},
-		{'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL};
+	fmt_t fmt_types[] = {
+		{'c', print_char}, {'s', print_string}, {'%', print_percent},
+		{'i', print_int}, {'d', print_int}, {'b', print_binary},
+		{'u', print_unsigned}, {'0', print_octal}, {'x', print_hexadecimal},
+		{'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable},
+		{'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
+	};
 
 	for (i = 0; fmt_types[i].fmt != '\0'; i++)
 
--- main.h
+++ main.h
@@ -64,11 +64,11 @@ int print_binary(va_list types, char buffer[],
 		int flags, int width, int precision, int size);
 int print_unsigned(va_list types, char buffer[],
 		int flags, int width, int precision, int size);
-int print_octa(va_list types, char buffer[],
+int print_octal(va_list types, char buffer[],
 		int flags, int width, int precision, int size);
 int print_hexadecimal(va_list types, char buffer[],
 		int flags, int width, int precision, int size);
-int print_hex_upper(va_list types, char buffer[],
+int print_hexa_upper(va_list types, char buffer[],
 		int flags, int width, int precision, int size);
 
 int print_hexa(va_list types, char map_to[],
